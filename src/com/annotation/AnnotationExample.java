e237caf5539beafbb833f4226bfec3c603a90cbb
Darpan Madlani <Darpan@proclink.com>
2022-01-04 21:18:50 +0530

    Annotation example

---
+++ src/com/annotation/AnnotationExample.java
@@ -0,0 +1,37 @@
+package com.annotation;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
+public class AnnotationExample {
+
+	@Deprecated
+	@MethodInfo(comment = "This is deprecated method, Don't use it", date = "4th Jan, 2022")
+	public void oldMethod() {
+		System.out.println("Old method decalration");
+	}
+
+	@Override
+	@MethodInfo(comment = "toString method overriden", date = "4th Jan, 2022")
+	public String toString() {
+		return "Demo str from AnnonationExample";
+	}
+
+	public static void main(String[] args) {
+
+		for (Method method : AnnotationExample.class.getMethods()) {
+			for (Annotation annotation : method.getAnnotations()) {
+				if (method.isAnnotationPresent(MethodInfo.class)) {
+					System.out.println(method + "\t" + annotation);
+					
+					MethodInfo methodInfo = method.getAnnotation(MethodInfo.class);
+					System.out.println("Author : "+methodInfo.author());
+					System.out.println("Revision : "+methodInfo.revision());
+					System.out.println("comment : "+methodInfo.comment());
+					System.out.println("date : "+methodInfo.date());
+				}
+			}
+		}
+
+	}
+}
---
+++ src/com/annotation/MethodInfo.java
@@ -0,0 +1,19 @@
+package com.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Target(ElementType.METHOD)
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MethodInfo {
+	public String author() default "Darpan";
+	public String date();
+	public int revision() default 1;
+	public String comment();
+}
